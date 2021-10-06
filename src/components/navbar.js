181a3031899ebb167f9cb072f944a9c2d60e17a8
JaveriaBaloch <streetback666@gmail.com>
2021-10-06 13:00:24 +0500

    awesome font is added to react

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,16 @@
 import { Component } from "react";
 import {Container,NavbarBrand} from "reactstrap"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
 import "../scss/Navbar.scss"
-
+const iconof = <FontAwesomeIcon icon={faClock} className="fa-4x text-success"/>
 class App extends Component{
    render() {
        return(
             <div className="navbar">
                 <Container>
-                    <NavbarBrand>Alarm-set-app</NavbarBrand>
-                    <i className="fas fa-clock fa-3x"></i>
+                    <NavbarBrand color="success">Alarm-set-app</NavbarBrand>
+                    {iconof}
                 </Container>
             </div>)
    }
